6ad373f96854c9a13b7f12c48f3b195be1f00e1f
MarwenTayari <tayarimarwen@gmail.com>
2022-01-22 16:24:16 +0100

    To update heroku app

--- client/src/actions/post.actions.js
+++ client/src/actions/post.actions.js
@@ -22,19 +22,19 @@ export const GET_POST_ERRORS = "GET_POST_ERRORS";
 
 export const getPost = (num) => {
     return (dispatch) => { 
-        return axios.get(`${process.env.REACT_APP_API_URL}api/post/`)
+        return axios.get(`${process.env.REACT_APP_API_URL}api/post`)
         .then((res) => { 
             const array = res.data.slice(0,num); 
             dispatch({type: GET_POSTS, payload : array }); 
             dispatch({type: GET_ALL_POSTS, payload : res.data }); 
         })
-        .catch((err)=> console.log(err))
+        .catch((err)=> console.log(err)) 
     }
 }
 
 export const addPost = (data) => {
     return (dispatch) => { 
-        return axios.post(`${process.env.REACT_APP_API_URL}api/post/`, data)
+        return axios.post(`${process.env.REACT_APP_API_URL}api/post`, data)
         .then((res)=> { 
             if(res.data.errors){
                 dispatch({ type : GET_POST_ERRORS , payload : res.data.errors })
