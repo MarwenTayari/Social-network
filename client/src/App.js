3237fb7d799e6c1dc1b535b6f3099fea1e3271e4
MarwenTayari <tayarimarwen@gmail.com>
2022-01-22 22:04:05 +0100

    To deploy project

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
            await axios({
                method:"get",
                url: `${process.env.REACT_APP_API_URL}jwtid` , 
-               withCredentials  : true , 
+               withCredentials  : true ,  
            })
            .then((res)=> {
                setUid(res.data); 
